d68243cd37b27ae82bc73e7f07f0cb6fe00c3957
Sardorbek <karimovsardor206@gmail.com>
2025-12-23 16:57:56 +0500

    fixed init sql

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -1,13 +1,11 @@
 -- GenixERP Database Initialization Script
 -- This script runs automatically when PostgreSQL container starts for the first time
+-- It runs on the database specified by POSTGRES_DB (genixerp)
 
 -- Create required extensions
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE EXTENSION IF NOT EXISTS "pg_trgm";
-
--- Grant privileges
-GRANT ALL PRIVILEGES ON DATABASE genixerp TO genix;
 
 -- Log initialization
 DO $$
