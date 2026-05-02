b0d27ed6ec6952ae74c77ea524330eb6938006d0
Behruz Niyozov <bniyozov73@gmail.com>
2026-05-02 12:29:17 +0500

    Show total consumption

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2025,6 +2025,7 @@ func (h *Handler) registerProtectedRoutes(rg *gin.RouterGroup) {
 		constructionProjects.GET("/:id/reports/summary", h.GetProjectSummaryReport)
 		constructionProjects.GET("/:id/reports/budget", h.GetStageBudgetReport)
 		constructionProjects.GET("/:id/reports/svod", h.GetSvodReport)
+		constructionProjects.GET("/:id/reports/material-consolidation", h.GetMaterialConsolidationReport)
 		constructionProjects.GET("/:id/reports/materials", h.GetMaterialsReport)
 		constructionProjects.GET("/:id/reports/journal-entries", h.GetJournalEntriesReport)
 
